c2aa353405c6782f0cea19bdd4b023e128b1b1c6
Jonathan Dahan <jonathan.dahan@buglabs.net>
2011-05-11 22:54:25 -0400

    fake lists of Inneract objects for learning how to add stuff to the map

--- src/us/inneract/Inneract.java
+++ src/us/inneract/Inneract.java
@@ -6,7 +6,19 @@ public class Inneract {
     private String action;
     private String description;
     private GeoPoint place;
-    private String person;
+    
+    // TODO: Add private Inneracter inneracter
+    
+    public Inneract(int longitude, int latitude, String action, String description) {
+        this.place = new GeoPoint(longitude, latitude);
+        this.action = action;
+        this.description = description;
+    }
+    
+    public String toString() {
+        return new String("hi i am an inneract!");
+    }
+    
     public void setAction(String action) {
         this.action = action;
     }
--- src/us/inneract/InneractMap.java
+++ src/us/inneract/InneractMap.java
@@ -1,12 +1,15 @@
 package us.inneract;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.LinearLayout;
 
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
 import com.google.android.maps.Overlay;
@@ -33,9 +36,18 @@ public class InneractMap extends MapActivity {
         drawable = this.getResources().getDrawable(R.drawable.act);
         itemizedOverlay = new InneractItemizedOverlay(drawable);
         
-        for ( Inneract i : inneracts )
+        inneracts = new HashSet<Inneract>();
+        inneracts.add(new Inneract(40752600, -73428200, null, null));
+        inneracts.add(new Inneract(40852600, -73528200, null, null));
+
+        for ( Inneract i : inneracts ) {
+            System.out.println(i);
+            Log.d(null, i.toString());
+            Log.d(NOTIFICATION_SERVICE, i.getPlace().toString());
+            
             itemizedOverlay.addOverlay(new OverlayItem(i.getPlace(), i.getAction(), i.getDescription()));
-        
+        }
+
         mapOverlays.add(itemizedOverlay);
 
     }
